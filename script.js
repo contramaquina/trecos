f7523980dcfea7741cad46a40526475e2ac39a4d
contramaquina <106447774+contramaquina@users.noreply.github.com>
2023-07-10 12:37:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Título da Minha Página</title>
+    </head>
+    <body>
+        <h1>Olá, Mundo!</h1>
+        <p>Esta é a minha primeira página HTML.</p>
+    </body>
+</html>
